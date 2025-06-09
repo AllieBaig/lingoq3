c03ce4b73ab3e0b69223f4708803b8228f1e49d7
AllieBaig <136852901+AllieBaig@users.noreply.github.com>
2025-06-09 15:42:40 +0100

    Create App.svelte

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+
+
+<script>
+  import MixLingo from './modes/MixLingo.svelte';
+</script>
+
+<main>
+  <h1>lingoq2 🌍</h1>
+  <MixLingo />
+</main>
+
+<style>
+  main {
+    text-align: center;
+    padding: 2rem;
+    font-family: sans-serif;
+  }
+</style>
+
